62f3a02896209ac701987905a0c65140da66aff9
ddshka <gorschynskyi@gmail.com>
2018-06-25 16:34:17 +0300

    staticfiles_dirs cleared

--- PartyCalculator/production_settings.py
+++ PartyCalculator/production_settings.py
@@ -145,7 +145,6 @@ STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, "static"),
 ]
 
 STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
