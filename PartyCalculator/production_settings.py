70338dffcbe4fe35b244acf631a944681e20f4b8
ddshka <gorschynskyi@gmail.com>
2018-06-25 16:04:11 +0300

    static_root added

--- PartyCalculator/production_settings.py
+++ PartyCalculator/production_settings.py
@@ -142,6 +142,7 @@ PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.0/howto/static-files/
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, "static"),
